fb6efba7eb992890af3afb5708ac2ce76e306eac
Pavel Reshetilo (aka) dp_ua <pavel.reshetilo@gmail.com>
2018-11-13 16:59:05 +0200

    add default messages

--- src/main/java/com/examples/goodmorning/message/TypeOfTime.java
+++ src/main/java/com/examples/goodmorning/message/TypeOfTime.java
@@ -1,5 +1,18 @@
 package com.examples.goodmorning.message;
 
 public enum TypeOfTime {
-    MORNIGN,DAY,EVENING,NIGHT;
+    MORNIGN("Good morning, World!"),
+    DAY("Good day, World!"),
+    EVENING("Good evening, World!"),
+    NIGHT("Good night, World!");
+
+    private String description;
+
+    TypeOfTime(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
