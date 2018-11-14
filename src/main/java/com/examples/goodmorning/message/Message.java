b0e7ddddbb2b77f0ad65633f338e559cdc86aba9
Pavel Reshetilo (aka) dp_ua <pavel.reshetilo@gmail.com>
2018-11-14 21:00:19 +0200

    Changed the locale search algorithm Made a loading class of standard messages in the absence of the necessary locale settings Added a check for the presence of a key in the external configuration files and in the internal class.

--- src/main/java/com/examples/goodmorning/message/Message.java
+++ src/main/java/com/examples/goodmorning/message/Message.java
@@ -15,7 +15,7 @@ import static com.examples.goodmorning.message.TypeOfTime.*;
 public class Message {
     private static final Logger log = Logger.getLogger(Message.class);
 
-    ResourceBundle resourceBundle = null;
+    private ResourceBundle resourceBundle = null;
 
     /**
      * Determines which type of time corresponds to the specified number of hours.
@@ -45,28 +45,45 @@ public class Message {
      * @param locale specified locale
      * @return String message to the specified time type in the language of the requested locale.
      * if nor find resource for need locale - load default messages
+     * @throws IllegalArgumentException When could not find message in external and internal resource files.
+     * Check the correctness of filling files MyMessages.properties or DefaultMessages.java class
      */
-    public String getMessage(TypeOfTime type, Locale locale) {
+
+
+    public String getMessage(TypeOfTime type, Locale locale) throws IllegalArgumentException{
         Locale defaultLocale = Locale.getDefault();
-        String result = null;
+        String result;
         try {
             Locale.setDefault(locale);
-            resourceBundle = ResourceBundle.getBundle("Messages");
+            resourceBundle = ResourceBundle.getBundle("MyMessages");
+            if (!resourceBundle.containsKey(type.toString())) {
+                log.error("Resource:  " + resourceBundle.getBaseBundleName() + " does not contains key:" + type.toString() + ". Loading default Resource Bundle");
+                resourceBundle = new DefaultMessages();
+            }
+        } catch (MissingResourceException mr) {
+            log.error("Locale " + locale.toString() + " not found. Loading default Resource Bundle");
+            resourceBundle = new DefaultMessages();
+        }
+        try {
             result = resourceBundle.getString(type.toString());
-            result = new String(result.getBytes("ISO-8859-1"), "UTF-8");
+        } catch (MissingResourceException mr) {
+            log.fatal("External and Locale Resource Bundle does not contain key: " + type.toString());
+            throw new IllegalArgumentException("Not found message for type:" + type.toString());
+        }
 
+        try {
+            result = new String(result.getBytes("ISO-8859-1"), "UTF-8");
         } catch (UnsupportedEncodingException e) {
             log.error(e.getMessage());
-        } catch (MissingResourceException mr) {
-            log.error("Locale " + locale.toString() + " not found. Loading default message");
-            result = type.getDescription();
         }
 
         log.debug("Type: " + type.toString() +
                 ", Locale: " + locale.toString() +
-                ", text message result: " + result);
+                ", text message result: " + result +
+                ", resourceBundle: " + resourceBundle.getBaseBundleName());
         Locale.setDefault(defaultLocale);
         return result;
     }
+
 
 }
